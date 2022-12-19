d19e489c5bb349899802ac3e0240e6d76f6b8206
Rainer Simon <rainer@rainersimon.io>
2022-12-19 09:24:52 +0100

    Build: added chunk splitting

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite';
+import { defineConfig, splitVendorChunkPlugin } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 export default defineConfig({
-  plugins: [ svelte({}) ],
+  plugins: [ svelte({}), splitVendorChunkPlugin() ],
   server: {
     open: '/test/index.html'
   },
